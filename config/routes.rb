14c35e97eb1f118b6c52d6ccd9c4ee99aefb46d5
Sutee Sudprasert <etipitaka@gmail.com>
2011-07-02 21:30:44 +0700

    Modified route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  match '/topics/:id/payment_require' => 'topics#payment_require?', :as => :topic_payment_require
   match '/contact-us' => 'contacts#new', :as => :contact
   
   resources :contacts, :controller => 'contacts'
