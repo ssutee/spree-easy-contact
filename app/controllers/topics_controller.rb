46a4da440ca7b7d803e05c858f51bb5a18b87704
Sutee Sudprasert <etipitaka@gmail.com>
2011-07-02 21:18:52 +0700

    Added order list

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,7 @@
+class TopicsController < Spree::BaseController
+  def payment_require?
+    if topic = Topic.find(params[:id])
+      render :json => topic.payment.to_json
+    end
+  end
+end
---
+++ db/migrate/20110701112239_add_payment_to_topics.rb
@@ -0,0 +1,9 @@
+class AddPaymentToTopics < ActiveRecord::Migration
+  def self.up
+    add_column :topics, :payment, :boolean
+  end
+
+  def self.down
+    remove_column :topics, :payment
+  end
+end
